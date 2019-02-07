b23d4bf316a68665b00c4debcc0522ca9979568c
LUNKUS <sigitas@lunkus.com>
2019-02-07 11:01:39 +0200

    modulo

---
+++ modulo/modulo.js
@@ -0,0 +1,13 @@
+
+    for (var i = 0; i < 100; i++) {
+        if (i % 2 == 0 && i % 3 == 0) {
+            console.log("Labas, Krabas");
+        } else if (i % 2 == 0 ) {
+            console.log("Labas");
+        } else if (i % 3 == 0 ) {
+            console.log("Krabas");
+        } else {
+            console.log(i);
+        }
+    }
+
