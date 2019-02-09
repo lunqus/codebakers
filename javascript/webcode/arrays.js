0643e959e14928aea1844ba337ddef48629ef2f8
LUNKUS <sigitas@lunkus.com>
2019-02-09 12:24:38 +0200

    arrays

--- javascript/webcode/arrays.js
+++ javascript/webcode/arrays.js
@@ -1,19 +1,26 @@
+// We have cars in a wide array of colors
+// Array is a collection made up of multiple items
+
 let myFavoriteNumber = 7
 let myFavoriteNumbers = [9, 2, 8, 3, 7, 4]
 let myWords = ["red", "orange", "yellow"]
-let myPets = [{name: "Meowsalot", species: "cat"}, {name: "Basksalot", species: "dog"}]
+let myPets = [{name: "Meowsalot", species: "cat"}, {name: "Barksalot", species: "dog"}]
 
-myWords.push("green")
-console.log(myWords)
-myWords.splice(1,1)
+myWords.push("green") // Add an item "green" onto the end of an existing array (collection)
 console.log(myWords)
 
-let myName = "Brad"
-console.log(myName.toUpperCase())
-
-let myNumber = 7.89
-console.log(myNumber.toFixed())
+myWords.splice(1,1) // Let us remove a particular item from an array (index of removing item, how many items form index)
+console.log(myWords)
 
 console.log(myFavoriteNumbers[2])
 
+console.log(myPets[1].name)
 console.log(myPets[1].species)
+
+
+
+let myName = "Brad"
+console.log(myName.toUpperCase() + " #" + myFavoriteNumber) // JavaScript treat string of text as if it was an object
+
+let myNumber = 7.89
+console.log(myNumber.toFixed()) // Same like with strings
