891ab0508a71551d04c5d30e597ae0ebd48f81ed
LUNKUS <sigitas@lunkus.com>
2019-02-09 15:59:32 +0200

    Higer-Order Functions

--- javascript/webcode/functions.js
+++ javascript/webcode/functions.js
@@ -20,3 +20,40 @@ alert(myFavoriteNumber)
 //  (A) Accepts a function as an argument
 //  (B) Returns a function as a result
 
+
+document.addEventListener("click", ourAmazingFunction) // (A)
+
+function ourAmazingFunction() {
+    alert("Thank you for clicking.")
+}
+
+
+function createMultiplier(multiplier) { // (B)
+    return function(x) {
+        return x * multiplier
+    }
+}
+
+let doubleMe = createMultiplier(2)
+let tripleMe = createMultiplier(3)
+let quadrupleMe = createMultiplier(4)
+
+
+console.log(doubleMe(20))
+console.log(tripleMe(20))
+console.log(quadrupleMe(20))
+
+
+// Higher-order functions that are part of the language itself
+
+let myColors = ['red', 'orange', 'yellow']
+
+myColors.forEach(saySomethingNice)
+
+function saySomethingNice(color) {
+    console.log("The color " + color + " is a great color.")
+}
+
+// Another higher-order functions
+myColors.map()
+myColors.filter()
