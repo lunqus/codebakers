8bfeef168e86683c82e5271f8ffc73bffda94005
LUNKUS <sigitas@lunkus.com>
2019-02-09 13:29:55 +0200

    decissions making

---
+++ javascript/webcode/makingDecisions.js
@@ -0,0 +1,15 @@
+let strawberryCount = 9
+
+if (strawberryCount > 9) {
+    // document.write("Congrats!")
+    console.log("Congrats!")
+} else {
+    // document.write("Sorry, we do not ship orders that small!")
+    console.log("Sorry, we do not ship orders that small!")
+}
+
+while (strawberryCount <= 15) {
+    // document.write("There are " + strawberryCount + " strawberries")
+    console.log("There are " + strawberryCount + " strawberries")
+    strawberryCount++
+}
