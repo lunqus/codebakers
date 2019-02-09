b8a4ac8a792f147e1e935e2e6bc180c2ab983b33
LUNKUS <sigitas@lunkus.com>
2019-02-09 20:19:37 +0200

    template literals

---
+++ javascript/webcode/templateLiterals.js
@@ -0,0 +1,9 @@
+
+let myName = "Brad"
+
+console.log("Hello, my name is " + myName + " and a sky is blue.")
+
+// To create a template literal we just wrap our text with back ticks instead of regular quotes
+// and include an expression ${}
+
+console.log(`Hello, my name is ${myName} and a sky is blue.`)
