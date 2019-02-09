4c73fa7c0845e81027983ab1c5604efa8d0e2baf
Sigitas Lunkus <sigitas@lunkus.com>
2019-02-09 08:51:00 +0200

    create arrays

---
+++ javascript/webcode/arrays.js
@@ -0,0 +1,19 @@
+let myFavoriteNumber = 7
+let myFavoriteNumbers = [9, 2, 8, 3, 7, 4]
+let myWords = ["red", "orange", "yellow"]
+let myPets = [{name: "Meowsalot", species: "cat"}, {name: "Basksalot", species: "dog"}]
+
+myWords.push("green")
+console.log(myWords)
+myWords.splice(1,1)
+console.log(myWords)
+
+let myName = "Brad"
+console.log(myName.toUpperCase())
+
+let myNumber = 7.89
+console.log(myNumber.toFixed())
+
+console.log(myFavoriteNumbers[2])
+
+console.log(myPets[1].species)
